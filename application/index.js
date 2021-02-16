066dcc7d80b803ac103bc199e5657f5d2e2748fe
James Coleman <jcolem22@msudenver.edu>
2021-02-16 12:58:54 -0700

    Added a docker file and began setting up an api

---
+++ application/index.js
@@ -0,0 +1,12 @@
+'use strict'
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.json([{
+	name: 'Eva',
+	email: 'eva@gmail.com'
+}]));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
