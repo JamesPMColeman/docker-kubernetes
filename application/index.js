ec570ff425b18d7472febfe04aeb7cce105c1542
James Coleman <jcolem22@msudenver.edu>
2021-02-16 13:44:32 -0700

    Rearanged the Docker file to enable more cacheing

--- application/index.js
+++ application/index.js
@@ -3,10 +3,24 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.json([{
-	name: 'Eva',
-	email: 'eva@gmail.com'
-}]));
+app.get('/', (req, res) => res.json([
+	{
+		name: 'Alec',
+		email: 'alec@yahoo.com'
+	}, 
+	{
+		name: 'Eva',
+		email: 'eva@gmail.com'
+	},
+	{
+		name: 'Bev',
+		email: 'bererly@anothersite.com'
+	}, 
+	{
+		name: 'Randy',
+		email: 'randy@msn.com'
+	} 
+]));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
